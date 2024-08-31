d1b2740dd564e7c52fcbd5eafcb5974e849d964c
prlgadminuser <liam.heizmann@gmail.com>
2024-08-31 05:56:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -52,7 +52,7 @@ const gamemodeconfig = {
     maxplayers: 1,
     respawns_allowed: 4,
     playerhealth: 77,
-    playerspeed: 0.24,           //(0.26 * server_tick_rate) / 17,
+    playerspeed: 0.26,           //(0.26 * server_tick_rate) / 17,
     zonespeed: 1.4,
     usezone: true,
     health_restore: true,
