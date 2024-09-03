6483efcc2fc943535058453c97f21a72c5d6ea16
prlgadminuser <liam.heizmann@gmail.com>
2024-09-03 06:12:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -134,11 +134,11 @@ const gunsconfig = {
     height: 5,
     useplayerangle: true,
     bullets: [
-      { angle: 0, speed: 45, delay: 0, offset: -10 },
-      { angle: 0, speed: 45, delay: 100, offset: 10 },
-      { angle: 0, speed: 45, delay: 200, offset: -10 },
-      { angle: 0, speed: 45, delay: 300, offset: 10 },
-      { angle: 0, speed: 45, delay: 400, offset: -10 }
+      { angle: 0, speed: 35, delay: 0, offset: -10 },
+      { angle: 0, speed: 35, delay: 100, offset: 10 },
+      { angle: 0, speed: 35, delay: 200, offset: -10 },
+      { angle: 0, speed: 35, delay: 300, offset: 10 },
+      { angle: 0, speed: 35, delay: 400, offset: -10 }
      // { angle: 0, speed: 25, distance: 300, delay: 500, offset: -10 }
      
     ]
