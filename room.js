d169f6132cb582f81218e657b2ab29733d6a8692
prlgadminuser <liam.heizmann@gmail.com>
2024-09-03 05:00:24 +0200

    Update room.js

--- room.js
+++ room.js
@@ -434,6 +434,8 @@ player.bullets.forEach(bullet => {
     ep: room.lastSent?.ep.length !== room.eliminatedPlayers.length ? room.eliminatedPlayers : undefined,
   };
 
+	console.log(room.lastSent?.ep.length, room.eliminatedPlayers.length);
+
   //pl: room.state === "playing" ? room.lastSent?.maxplayers !== room.maxplayers ? { pl: room.maxplayers } : {} : room.maxplayers,
 
   const jsonString = JSON.stringify(newMessage);
