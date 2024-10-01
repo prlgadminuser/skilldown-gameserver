b4058c30d3163a73837553766cd598dc37dec7c1
prlgadminuser <liam.heizmann@gmail.com>
2024-10-01 14:14:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -173,8 +173,13 @@ const gunsconfig = {
     useplayerangle: true,
     bullets: [
       { angle: 0, speed: 35, delay: 0, offset: 0 },
-     // { angle: 0, speed: 25, distance: 300, delay: 500, offset: -10 }
-     
+      { angle: 0, speed: 35, delay: 100, offset: -10 }
+         { angle: 0, speed: 35, delay: 200, offset: 10 }
+    { angle: 0, speed: 35, delay: 0, offset: 0 },
+      { angle: 0, speed: 35, delay: 100, offset: -10 }
+         { angle: 0, speed: 35, delay: 200, offset: 10 }
+      
+
     ],
     damageconfig: [
       { threshold: 45, damageMultiplier: 1 },    // Layer 1: Full damage if within 25% of max distance
