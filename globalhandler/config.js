77e00f9a082ec92cae40f9a380837cb3ce8e9123
prlgadminuser <liam.heizmann@gmail.com>
2024-12-08 01:48:45 +0100

    Update config.js

--- globalhandler/config.js
+++ globalhandler/config.js
@@ -282,7 +282,7 @@ class SpatialGrid {
 }
 
 // Initialize grids for all maps
-const cellSize = 300; // Adjust as necessary
+const cellSize = 100; // Adjust as necessary
 Object.keys(mapsconfig).forEach(mapKey => {
   const map = mapsconfig[mapKey];
   const grid = new SpatialGrid(cellSize);
