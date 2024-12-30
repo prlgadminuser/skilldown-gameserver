4bb4ec75a825bdb85832e551b2a43f53fec6ba5b
prlgadminuser <liam.heizmann@gmail.com>
2024-12-30 04:44:37 +0100

    Update bullets.js

--- globalhandler/bullets.js
+++ globalhandler/bullets.js
@@ -82,7 +82,7 @@ function moveBullet(room, player, bullet) {
 
       const potentialTargets = Array.from(room.players.values()).filter(otherPlayer => 
         otherPlayer !== player &&  // Exclude the player themselves
-        otherPlayer.visible &&     // Only consider visible players
+        otherPlayer.visible //&&     // Only consider visible players
       //  !player.team.players.some(teamPlayer => teamPlayer.playerId === otherPlayer.playerId)  // Exclude teammates
       );
 
