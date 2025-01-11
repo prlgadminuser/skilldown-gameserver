e91f4b7b3e901ebdfc4268bed8c884bdd5ddb4d5
prlgadminuser <liam.heizmann@gmail.com>
2025-01-11 02:03:55 +0100

    Delete globalhandler/tempCodeRunnerFile.js

--- globalhandler/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-filteredplayers = Object.entries(playerData).reduce((result, [playerId, playerData]) => {
-        if (playersInRange.has(Number(playerId))) {
-          result[playerId] = playerData; // Add filtered player data
-        }
-        return result;
-      }, {});
