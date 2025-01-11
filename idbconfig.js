aa5bd29d36475fbf47092cf92564b04740888cb9
prlgadminuser <liam.heizmann@gmail.com>
2025-01-11 22:43:19 +0100

    Update idbconfig.js

--- idbconfig.js
+++ idbconfig.js
@@ -3,10 +3,13 @@
 const password = process.env.DB_KEY || "8RLj5Vr3F6DRBAYc"
 const encodedPassword = encodeURIComponent(password);
 
+const tokenkey = "d8ce40604d359eeb9f2bff31beca4b4b"
+
 const lgconnecturi = `mongodb+srv://Liquem:${encodedPassword}@cluster0.ed4zami.mongodb.net/?retryWrites=true&w=majority`;
 
 const uri = lgconnecturi
 
 module.exports = {
-   uri
+   uri,
+   tokenkey,
 }
