f21bd7817d251baa71797b610ed1ce7e6937da77
prlgadminuser <liam.heizmann@gmail.com>
2025-02-05 18:56:15 +0100

    Update config.js

--- globalhandler/config.js
+++ globalhandler/config.js
@@ -87,7 +87,7 @@ const gamemodeconfig = {
     maxplayers: 1,
     respawns_allowed: 1,
     playerhealth: 9999,
-    playerspeed: 0.14,
+    playerspeed: 0.1,
     usezone: false,
     zonespeed: 0.8,
     health_restore: false,
