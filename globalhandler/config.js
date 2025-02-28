ca614b7cccc836bc78a83921a3f5e4dfc04b2896
prlgadminuser <liam.heizmann@gmail.com>
2025-02-28 06:25:48 +0100

    Update config.js

--- globalhandler/config.js
+++ globalhandler/config.js
@@ -3,7 +3,7 @@
 const rooms = new Map();
 
 const gridcellsize = 100; 
-const server_tick_rate = 17 //17
+const server_tick_rate = 16.4 //17
 const matchmaking_timeout = 120000
 const player_idle_timeout = 10000
 const game_start_time = 1000
